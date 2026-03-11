21e087ee2fd3fdc45a67ff95892514c7e9efc0b2
yuvalheby <yuval41002@gmail.com>
2026-03-11 14:59:34 +0200

    Create base TB

---
+++ avalon_st_agent_tb.sv
@@ -0,0 +1,51 @@
+// -----------------------------------------------------------------------------
+// File        : avalon_st_agent_tb.sv
+// Author      : 
+// Description : Top TB module for Agent Exercise.
+// -----------------------------------------------------------------------------
+
+module tb ();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Parameters.
+    //////////////////////////////////////////////////////////////////////////////
+    // Data width.
+    localparam int unsigned DATA_WIDTH_IN_BYTES = 4;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Declarations.
+    //////////////////////////////////////////////////////////////////////////////
+    // Clock and reset.
+    bit clk;
+    bit rst_n;
+
+    // Interface declaration.
+    avalon_st_if#(.DATA_WIDTH_IN_BYTES(DATA_WIDTH_IN_BYTES)) vif (.clk(clk));
+
+    // TODO - Declare your classes here.
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Clock and Reset Toggling.
+    //////////////////////////////////////////////////////////////////////////////
+    // Generate clock.
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk; 
+    end
+
+    // Initialize reset signal.
+    initial begin
+        rst_n = 0;
+        #20;
+        rst_n = 1;
+    end
+
+    //////////////////////////////////////////////////////////////////////////////
+    // TestBench Logic
+    //////////////////////////////////////////////////////////////////////////////
+    // Test logic.
+    initial begin
+    	
+    	// TODO - Insert TB logic here.
+    end
+endmodule
