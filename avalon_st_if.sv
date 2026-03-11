558074b9766ef13c8c2f536c9a96f7d324b74881
yuvalheby <yuval41002@gmail.com>
2026-03-11 14:27:03 +0200

    Add avalon_st_if

---
+++ avalon_st_if.sv
@@ -0,0 +1,94 @@
+// -----------------------------------------------------------------------------
+// File        : avalon_st_if.sv
+// Author      : Yuval Heby
+// Description : Avalon Streaming Interface class.
+// -----------------------------------------------------------------------------
+
+interface avalon_st_if #(int unsigned DATA_WIDTH_IN_BYTES = 4)(input logic clk);
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // Interface Signals.
+    //////////////////////////////////////////////////////////////////////////////
+    logic                                             valid;
+    logic                                             rdy;
+    logic                                             sop;
+    logic                                             eop;
+    logic [DATA_WIDTH_IN_BYTES * $bits(byte) - 1 : 0] data;
+    logic [$clog2(DATA_WIDTH_IN_BYTES)       - 1 : 0] empty;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Modports.
+    //////////////////////////////////////////////////////////////////////////////
+    // Master.
+    modport master (
+        input  clk,
+        input  rdy,
+        output valid,
+        output sop,
+        output eop,
+        output data,
+        output empty
+    );
+
+    // Slave.
+    modport slave (
+        input  clk,
+        input  valid,
+        input  sop,
+        input  eop,
+        input  data,
+        input  empty,
+        output rdy
+    );
+
+    // Monitor.
+    modport monitor (
+        input  clk,
+        input  valid,
+        input  rdy,
+        input  sop,
+        input  eop,
+        input  data,
+        input  empty
+    );
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Clocking Blocks.
+    //////////////////////////////////////////////////////////////////////////////
+    // Master Clocking Block.
+    clocking master_cb @(posedge clk);
+        default input #1step output #1ns;
+        input  rdy;
+        output valid, sop, eop, data, empty;
+    endclocking
+
+    // Slave Clocking Block.
+    clocking slave_cb @(posedge clk);
+        default input #1step output #1ns;
+        input  valid, sop, eop, data, empty;
+        output rdy;
+    endclocking
+
+    // Monitor Clocking Block.
+    clocking monitor_cb @(posedge clk);
+        default input #1step;
+        input valid, rdy, sop, eop, data, empty;
+    endclocking
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Methods.
+    //////////////////////////////////////////////////////////////////////////////
+    // Clears the Master clocking block signals
+    function void CLEAR_MASTER_CB();
+        cb_master.valid <= 1'b0;
+        cb_master.sop   <= 1'b0;
+        cb_master.eop   <= 1'b0;
+        cb_master.data  <= '0;
+        cb_master.empty <= '0;
+    endfunction
+
+    // Clears the Slave clocking block signals
+    function void CLEAR_SLAVE_CB();
+        cb_slave.rdy <= 1'b0;
+    endfunction
+endinterface
